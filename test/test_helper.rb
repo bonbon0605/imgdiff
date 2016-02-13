9ad8eb78ff4e51b0280dfd0a769ea6bf3b3aaa04
bonbon0605 <ogasawara_2005@yahoo.co.jp>
2016-02-14 00:12:35 +0900

    add some basic tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,16 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'imgdiff'
 
 require 'minitest/autorun'
+def images
+  {
+    original: 'test/images/original.jpg',
+    target: 'test/images/target.jpg',
+    composite: 'test/images/composite.jpg'
+  }
+end
+
+def same_image?(original,target)
+  return false unless original.instance_of? Magick::Image and target.instance_of? Magick::Image
+  return true if original.difference(target) == [0.0,0.0,0.0]
+  return false
+end
