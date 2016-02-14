5ce8b1898ea794a66a1ad2a877624d6832ec225e
bonbon0605 <ogasawara_2005@yahoo.co.jp>
2016-02-14 16:15:02 +0900

    modify the way to delete test files

--- test/imgdiff_test.rb
+++ test/imgdiff_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class ImgdiffTest < Minitest::Test
+  def teardown
+    Dir.glob('test/images/*').each do |file|
+      File.delete file unless images[:original] == file or images[:target] == file or images[:composite] == file
+    end
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Imgdiff::VERSION
   end
@@ -11,8 +17,6 @@ class ImgdiffTest < Minitest::Test
 
     assert File.exist? output
     assert same_image? Magick::Image.read(images[:composite]).first, Magick::Image.read(output).first
-
-    system "rm #{output}"
   end
 
   def test_that_it_makes_output_at_the_path_directed_by_3rd_argument
@@ -24,9 +28,6 @@ class ImgdiffTest < Minitest::Test
 
     assert File.exist? output_one
     assert File.exist? output_another
-
-    system "rm #{output_one}"
-    system "rm #{output_another}"
   end
 
   def test_that_when_3rd_argument_is_not_given_it_makes_output_path_from_two_files_name_with_timestamp
@@ -34,7 +35,6 @@ class ImgdiffTest < Minitest::Test
     ImgDiff.new([images[:original],images[:target]]).invoke(:exec)
     output = Dir.glob "#{expect}[0-9]*#{File.extname(images[:original])}"
     assert !output.empty?
-    output.each{|file| system "rm #{file}"}
   end
 
   def test_that_when_image_file_not_exist_it_ends_with_exception
@@ -57,8 +57,6 @@ class ImgdiffTest < Minitest::Test
 
     ImgDiff.new([images[:original],images[:composite],output]).invoke(:exec)
     assert_equal false, same_image?(Magick::Image.read(output).first, Magick::Image.read(images[:composite]).first)
-
-    system "rm #{output}"
   end
 
   def test_it_does_not_make_diff_file_when_there_is_no_diff_between_two_files
